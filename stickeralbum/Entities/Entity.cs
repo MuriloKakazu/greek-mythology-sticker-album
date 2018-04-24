a5800fbec7b1dac279bfe57f3b0fa60a8ec75c25
Murilo Pires <mupapi@hotmail.com>
2018-04-23 22:02:13 -0300

    Changezzzzz

---
+++ stickeralbum/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stickeralbum.Entities {
+    public abstract class Entity {
+        public Int32 ID { get; set; }
+        public String ImagePath { get; set; }
+        public String Name { get; set; }
+        public Rarity Rarity { get; set; }
+        public String Description { get; set; }
+
+        public override string ToString() => this.Name;
+    }
+}
---
+++ stickeralbum/IO/Cache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using stickeralbum.Entities;
+using stickeralbum.Generics;
+
+namespace stickeralbum.IO
+{
+    public static class Cache {
+
+        public class Data {
+        }
+
+        static Cache() {
+
+        }
+    }
+}
