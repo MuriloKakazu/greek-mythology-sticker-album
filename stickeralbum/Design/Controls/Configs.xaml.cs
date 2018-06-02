504737acf9210c7c3e88c0cf4cc8c58bb5e7a8d8
Murilo Parra Pires <mupapi@hotmail.com>
2018-06-02 01:44:46 -0300

    Settings

---
+++ stickeralbum/Design/Controls/Configs.xaml.cs
@@ -0,0 +1,50 @@
+﻿using stickeralbum.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stickeralbum.Design.Controls {
+    /// <summary>
+    /// Interação lógica para Configs.xam
+    /// </summary>
+    public partial class Configs : UserControl {
+
+        public float volume;
+
+        public Configs() {
+            InitializeComponent();
+            volume = Game.GameMaster.Settings.Volume;
+        }
+
+        private void CheckBoxMuted_Click(object sender, RoutedEventArgs e) {
+            SliderVolume.Value = CheckBoxMuted.IsChecked.Value ? 0 : volume;
+        }
+
+        private void ButtonRestore_Click(object sender, RoutedEventArgs e) {
+            File.WriteAllText(Paths.CustomCreaturesMetadata, "[\n\n]");
+            File.WriteAllText(Paths.CustomSemiGodsMetadata, "[\n\n]");
+            File.WriteAllText(Paths.CustomGodsMetadata, "[\n\n]");
+            File.WriteAllText(Paths.CustomTitansMetadata, "[\n\n]");
+            File.WriteAllText(Paths.CustomSpritesMetadata, "[\n\n]");
+            DirectoryInfo di = new DirectoryInfo(Paths.CustomSpritesDirectory);
+            Game.GameMaster.Player.Inventory.RemoveCustoms();
+            Cache.Clear();
+            foreach(FileInfo file in di.GetFiles()) {
+                file.Delete();
+            }
+            Cache.Load();
+        }
+    }
+}
--- stickeralbum/Design/Controls/SidebarMenu.xaml.cs
+++ stickeralbum/Design/Controls/SidebarMenu.xaml.cs
@@ -102,6 +102,7 @@ namespace stickeralbum.Design.Controls {
 
         private void OptionsButton_MouseDown(object sender, MouseButtonEventArgs e) {
             ChangeState(ControlState.Compact);
+            App.ClientWindow.SetCurrentPage(new Configs());
         }
     }
 }
